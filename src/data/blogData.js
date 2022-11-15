b43b0cfb7e903761095a39ab784ae349ce1c5049
LBaileyIBM <lewis.bailey-cic@ibm.com>
2022-11-15 16:34:29 +0000

    small update v3

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,37 +1,36 @@
 export const blogData = [
-    {
-        id: 1,
-        title: 'Cloning Sportsjam.in',
-        description: 'My expeirence working on my first big project within the Masai Curriculum',
-        date: 'Dec 19, 2021',
-        image: 'https://miro.medium.com/max/875/1*R__cdn8L1gYGK1Tdqqyubg.png',
-        url: 'https://medium.com/@sreerag.rajan5/cloning-sportsjam-in-cd1c80c1b06b'
-    },
-    {
-        id: 2,
-        title: 'CLONING RELIANCE DIGITAL',
-        description: 'Experience of the second project.',
-        date: 'Jan 23, 2022',
-        image: 'https://miro.medium.com/max/875/1*nY9cUfzwtDaM6lGyvj4STg.png',
-        url: 'https://medium.com/@sreerag.rajan5/cloning-reliance-digital-439cd56d46a3'
-    },
-    {
-        id: 3,
-        title: 'Reliance Digital Clone (Part II)',
-        description: 'The experience of the third project, with a focus on leadership skills',
-        date: 'Feb 27, 2022',
-        image: 'https://miro.medium.com/max/875/1*qRNfRbuHCDC1ct0chLZoWA.png',
-        url: 'https://medium.com/@sreerag.rajan5/reliance-digital-clone-part-ii-57797eaf1ddf'
-    },
-    
-]
-
-
-
+  {
+    id: 1,
+    title: '8 week web development fundamentals',
+    description: 'Master key web technologies; HTML, CSS, Javascript',
+    date: 'December, 2022',
+    image:
+      'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200203200336/13-Things-You-Should-Know-Before-You-Enter-In-Web-Development.png',
+    url: 'https://medium.com/@sreerag.rajan5/cloning-sportsjam-in-cd1c80c1b06b',
+  },
+  {
+    id: 2,
+    title: 'Intermediate web development',
+    description:
+      'For those who are familiar with Javascript and want to become job interview ready',
+    date: 'March, 2023',
+    image: 'https://miro.medium.com/max/1400/1*OUjorKRqq2uWsBPXsuna6g.png',
+    url: 'https://medium.com/@sreerag.rajan5/cloning-reliance-digital-439cd56d46a3',
+  },
+  {
+    id: 3,
+    title: 'Japanese for beginners',
+    description:
+      'A short course to cover basic Japanese topics to JLPT N4 level',
+    date: 'January, 2023',
+    image:
+      'https://t4.ftcdn.net/jpg/00/33/86/49/360_F_33864906_TUI2XJHIumrdXupgxHcEpJ2EfJ3TddPx.jpg',
+    url: 'https://medium.com/@sreerag.rajan5/reliance-digital-clone-part-ii-57797eaf1ddf',
+  },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below.
-
 
 /* 
 {

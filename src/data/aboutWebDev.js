92d801753dcd76b26b73e8e39d5a55eb44c5ce3a
Lewis Bailey <lewis_bailey@hotmail.com>
2023-07-20 11:59:08 +0100

    updating about me

--- src/data/aboutWebDev.js
+++ src/data/aboutWebDev.js
@@ -3,8 +3,8 @@ export const aboutWebDev = {
   description1:
     'Web development typically refers to the coding and programming side of website production.',
   description2:
-    'When you learn web development, you might start out writing a simple page of HTML text and build up to creating more complex, feature-rich applications designed to be accessed from various Internet-connected devices.',
+    'When you learn web development, you might start out writing a simple page of HTML text and build up to creating more complex, feature-rich applications designed to be accessed from various devices.',
   description3:
-    'Examples of feature-rich web development include e-commerce websites, content management systems (CMS), and social networks',
+    'Examples of feature-rich web development include blogs, shopping websites, company portals, and social networks',
   image: 1,
 };
